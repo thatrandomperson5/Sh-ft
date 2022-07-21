d3511bc389cb84701f617f447052fa00bd276d75
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-07-21 19:00:27 -0400

    Update index.js

--- docs/sources/index.js
+++ docs/sources/index.js
@@ -1,1 +1,10 @@
-
+fetch('https://discord.com/api/guilds/999798556644868196/widget.json')
+  .then(response => response.json())
+  .then(data => {
+   const status = Array.from(document.getElementsByClassName("status-wrap"))
+   
+   status.forEach(tag => {
+      
+      tag.innerHTML = `<img class="circle-image" src=${data["members"][1]["avatar_url"]}><b class="wtext">&nbsp;${data["members"][1]["username"]} &bull; ${data["members"][1]["status"]}</b>`
+   });
+  });
