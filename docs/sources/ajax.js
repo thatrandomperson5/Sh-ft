357fda106572d1d2d5a3a5c9811438a5d147bb15
BuildTools <unconfigured@null.spigotmc.org>
2022-07-22 12:16:19 -0400

    Commit

--- docs/sources/ajax.js
+++ docs/sources/ajax.js
@@ -1,11 +1,14 @@
 async function pre_rend(page) {
     var xhttp = new XMLHttpRequest();
     xhttp.onload =  () => {
+        console.log(xhttp.responseURL)
         document.write(this.responseText);
+        
     }
     xhttp.open("GET", page, true);
     xhttp.send();
+    
 }
 window.addEventListener("hashchange", () => {
-    console.log(location.hash.slice(1))
+    pre_rend(`${location.hash.slice(1)}`)
 });
