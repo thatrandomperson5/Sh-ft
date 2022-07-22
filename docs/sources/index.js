a53b1d67683b9ba3095378ba600606ccc0893bc2
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-07-21 20:00:10 -0400

    Update index.js

--- docs/sources/index.js
+++ docs/sources/index.js
@@ -8,3 +8,17 @@ fetch('https://discord.com/api/guilds/999798556644868196/widget.json')
       tag.innerHTML = `<img class="circle-image" src=${data["members"][1]["avatar_url"]}><b class="wtext">&nbsp;${data["members"][1]["username"]} &bull; ${data["members"][1]["status"]}</b>`
    });
   });
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 5000);
+}
