279d36e9734c0ad03dd76f45962a3c9ce483bcde
BuildTools <unconfigured@null.spigotmc.org>
2022-07-23 14:46:12 -0400

    Commit

--- css/init.js
+++ css/init.js
@@ -1,8 +1,25 @@
 
 //Creation of the little side bar thing on blockquotes.
-var js_auto_blocks = document.getElementsByTagName("blockquote")
-js_auto_blocks = Array.from(js_auto_blocks)
-js_auto_blocks.forEach((qut) => {
+var discord_auto_blocks = document.querySelectorAll(".discord-msg blockquote")
+discord_auto_blocks = Array.from(discord_auto_blocks)
+console.log(discord_auto_blocks)
+discord_auto_blocks.forEach((qut) => {
+	var node = document.createElement("div")
+  node.setAttribute("class","discord-msg-wrap")
+  var node2 = document.createElement("div")
+  node2.innerHTML = "&nbsp;"
+  node2.setAttribute("class","discord-msg-block")
+  
+	
+  node.appendChild(node2)
+  node.innerHTML += `<blockquote>`+qut.innerHTML+`</blockquote>`
+  qut.parentNode.replaceChild(node, qut)
+  
+});
+var discord_auto_blocks = document.querySelectorAll(".discord-msg blockquote")
+discord_auto_blocks = Array.from(discord_auto_blocks)
+console.log(discord_auto_blocks)
+discord_auto_blocks.forEach((qut) => {
 	var node = document.createElement("div")
   node.setAttribute("class","discord-msg-wrap")
   var node2 = document.createElement("div")
