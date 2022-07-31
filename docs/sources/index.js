642c53d0aa5cddfaa4bdd18b3c641c914feb0b7b
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-07-31 18:54:22 -0500

    Update index.js

--- docs/sources/index.js
+++ docs/sources/index.js
@@ -11,7 +11,7 @@ fetch('https://discord.com/api/guilds/999798556644868196/widget.json')
   });
 }
 var myIndex = 0;
-
+var myIndex2 = 0;
 carousel2(); 
 carousel();
 index();
@@ -28,14 +28,15 @@ async function carousel() {
   x[myIndex-1].style.display = "block";
   setTimeout(carousel, 5000);
 }
+
 async function carousel2() {
   var i;
   var x = document.getElementsByClassName("mySlides2");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
+  myIndex2++;
+  if (myIndex2 > x.length) {myIndex2 = 1}
+  x[myIndex2-1].style.display = "block";
   setTimeout(carousel, 5000);
 }
