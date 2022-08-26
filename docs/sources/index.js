9a9f99afb7c9b3d8a8d6ccac0143332bd73a3ed5
thatrandomperson5 <thatrandomperson5@users.noreply.github.com>
2022-08-26 14:07:12 +0000

    :art: Format code with prettier

--- docs/sources/index.js
+++ docs/sources/index.js
@@ -1,21 +1,19 @@
-async function index () {
-fetch('https://discord.com/api/guilds/999798556644868196/widget.json')
-  .then(response => response.json())
-  .then(data => {
-   const status = Array.from(document.getElementsByClassName("status-wrap"))
-   
-   status.forEach(tag => {
-      
-      tag.innerHTML = `<img class="circle-image" src=${data["members"][1]["avatar_url"]}><b class="wtext">&nbsp;${data["members"][1]["username"]} &bull; ${data["members"][1]["status"]}</b>`
-   });
-  });
+async function index() {
+  fetch("https://discord.com/api/guilds/999798556644868196/widget.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const status = Array.from(document.getElementsByClassName("status-wrap"));
+
+      status.forEach((tag) => {
+        tag.innerHTML = `<img class="circle-image" src=${data["members"][1]["avatar_url"]}><b class="wtext">&nbsp;${data["members"][1]["username"]} &bull; ${data["members"][1]["status"]}</b>`;
+      });
+    });
 }
 var myIndex = 0;
 var myIndex2 = 0;
-carousel2(); 
+carousel2();
 carousel();
 //index();
-
 
 async function carousel() {
   var i;
@@ -24,8 +22,10 @@ async function carousel() {
     x[i].style.display = "none";
   }
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
   setTimeout(carousel, 5000);
 }
 
@@ -36,7 +36,9 @@ async function carousel2() {
     x2[i2].style.display = "none";
   }
   myIndex2++;
-  if (myIndex2 > x2.length) {myIndex2 = 1}
-  x2[myIndex2-1].style.display = "block";
+  if (myIndex2 > x2.length) {
+    myIndex2 = 1;
+  }
+  x2[myIndex2 - 1].style.display = "block";
   setTimeout(carousel2, 5000);
 }
